ce137773e6166602f3d8cf0b99651277d1fab4c0
Dmitry Nedospasov <nedos@compute3.ai>
2025-12-09 23:41:48 +0300

    Fix phantom

--- site/packages/shared-ui/src/types/global.d.ts
+++ site/packages/shared-ui/src/types/global.d.ts
@@ -2,11 +2,22 @@ interface EthereumProvider {
   request: (args: { method: string; params?: any[] }) => Promise<any>;
   on: (event: string, handler: (...args: any[]) => void) => void;
   removeListener: (event: string, handler: (...args: any[]) => void) => void;
+  isMetaMask?: boolean;
+  isPhantom?: boolean;
+  selectedAddress?: string | null;
+  chainId?: string;
+}
+
+interface PhantomEthereum extends EthereumProvider {
+  isPhantom: true;
 }
 
 declare global {
   interface Window {
     ethereum?: EthereumProvider;
+    phantom?: {
+      ethereum?: PhantomEthereum;
+    };
   }
 }
 
